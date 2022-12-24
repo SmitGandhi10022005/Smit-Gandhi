adca7e83bb8ba7ed390b83a0d7343599315be837
Smit <22CE031@charusat.edu.in>
2022-12-24 13:58:57 +0530

    Commit

---
+++ struct.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+struct book
+{
+char Name[20];
+int price;
+}B;
+int main()
+printf("Enter the name of book");
+scanf("%[^\n]s",B.Name);
+printf("Enter the price of book");
+scanf("%d",&B.price);
+printf("This program is prepared by smit 22ce031");
+}
